aae5792476f2a170a0f2630e8eafc39899ce1f98
root <root@smtp.gmail.com>
2013-10-30 16:15:26 +0500

    u

--- src/com/veriqual/gofast/utilites/Utilities.java
+++ src/com/veriqual/gofast/utilites/Utilities.java
@@ -349,45 +349,4 @@ public class Utilities {
 		return list;
 		
 	}
-	
-	public static ArrayList<HashMap> populateList() {
-		 
-		ArrayList<HashMap> list = new ArrayList<HashMap>();
- 
-        HashMap temp = new HashMap();
-            temp.put(Constant.FIRST_COLUMN,"Colored Notebooks");
-            temp.put(Constant.SECOND_COLUMN, "By NavNeet");
-            temp.put(Constant.THIRD_COLUMN, "Rs. 200");
-            temp.put(Constant.FOURTH_COLUMN, "Per Unit");
-        list.add(temp);
- 
-        HashMap temp1 = new HashMap();
-            temp1.put(Constant.FIRST_COLUMN,"Diaries");
-            temp1.put(Constant.SECOND_COLUMN, "By Amee Products");
-            temp1.put(Constant.THIRD_COLUMN, "Rs. 400");
-            temp1.put(Constant.FOURTH_COLUMN, "Per Unit");
-        list.add(temp1);
- 
-        HashMap temp2 = new HashMap();
-            temp2.put(Constant.FIRST_COLUMN,"Note Books and Stationery");
-            temp2.put(Constant.SECOND_COLUMN, "By National Products");
-            temp2.put(Constant.THIRD_COLUMN, "Rs. 600");
-            temp2.put(Constant.FOURTH_COLUMN, "Per Unit");
-        list.add(temp2);
- 
-        HashMap temp3 = new HashMap();
-            temp3.put(Constant.FIRST_COLUMN,"Corporate Diaries");
-            temp3.put(Constant.SECOND_COLUMN, "By Devarsh Prakashan");
-            temp3.put(Constant.THIRD_COLUMN, "Rs. 800");
-            temp3.put(Constant.FOURTH_COLUMN, "Per Unit");
-        list.add(temp3);
- 
-        HashMap temp4 = new HashMap();
-            temp4.put(Constant.FIRST_COLUMN,"Writing Pad");
-            temp4.put(Constant.SECOND_COLUMN, "By TechnoTalaktive Pvt. Ltd.");
-            temp4.put(Constant.THIRD_COLUMN, "Rs. 100");
-            temp4.put(Constant.FOURTH_COLUMN, "Per Unit");
-        list.add(temp4);
-		return list;
-    }
 }
